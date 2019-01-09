66b8fb421f8e74cb816cdaa5729a65533369cb5f
AlbertRD <flashinvoice@gmail.com>
2019-01-09 13:12:55 +0100

    Creation

---
+++ admin/language/en-gb/catalog/category.php
@@ -0,0 +1,37 @@
+<?php
+// Heading
+$_['heading_title']          = 'Category';
+
+// Text
+$_['text_success']           = 'Success: You have modified categories!';
+$_['text_default']           = 'Default';
+$_['text_image_manager']     = 'Image Manager';
+$_['text_browse']            = 'Browse';
+$_['text_clear']             = 'Clear';
+
+// Column
+$_['column_name']            = 'Category Name';
+$_['column_sort_order']      = 'Sort Order';
+$_['column_action']          = 'Action';
+
+// Entry
+$_['entry_name']             = 'Category Name:';
+$_['entry_meta_keyword'] 	 = 'Meta Tag Keywords:';
+$_['entry_meta_description'] = 'Meta Tag Description:';
+$_['entry_description']      = 'Description:';
+$_['entry_parent']           = 'Parent:';
+$_['entry_filter']           = '<a data-toggle="tooltip" title="(Autocomplete)"><i class="fas fa-question-circle"></i></a> Filters';
+$_['entry_store']            = 'Stores:';
+$_['entry_keyword']          = '<a data-toggle="tooltip" title="Do not use spaces instead replace spaces with - and make sure the keyword is globally unique."><i class="fas fa-question-circle"></i></a> SEO Keyword';
+$_['entry_image']            = 'Image:';
+$_['entry_top']              = '<a data-toggle="tooltip" title="Display in the top menu bar. Only works for the top parent categories."><i class="fas fa-question-circle"></i></a> Top';
+$_['entry_column']           = '<a data-toggle="tooltip" title="Number of columns to use for the bottom 3 categories. Only works for the top parent categories."><i class="fas fa-question-circle"></i></a> Columns';
+$_['entry_sort_order']       = 'Sort Order:';
+$_['entry_status']           = 'Status:';
+$_['entry_layout']           = 'Layout Override:';
+
+// Error 
+$_['error_warning']          = 'Warning: Please check the form carefully for errors!';
+$_['error_permission']       = 'Warning: You do not have permission to modify categories!';
+$_['error_name']             = 'Category Name must be between 2 and 32 characters!';
+?>
