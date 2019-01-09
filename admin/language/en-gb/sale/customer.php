2de396cd3ecbeb0d06fb9cb969836c4cb47214b4
AlbertRD <flashinvoice@gmail.com>
2019-01-09 13:19:38 +0100

    Create customer

---
+++ admin/language/en-gb/sale/customer.php
@@ -0,0 +1,215 @@
+<?php
+// Heading
+$_['heading_title']         = 'Customer';
+$_['heading_contact']		= 'Customer Contact';
+
+
+// Text
+$_['text_success']          = 'Success: You have modified customers!';
+$_['text_default']          = 'Default';
+$_['text_approved']         = 'You have approved %s accounts!';
+$_['text_wait']             = 'Please Wait!';
+$_['text_balance']          = 'Balance:';
+$_['text_add_ban_ip']       = 'Add Ban IP';
+$_['text_remove_ban_ip']    = 'Remove Ban IP';
+
+
+// Tabs
+$_['tab_email']             = 'Emails';
+$_['tab_products']          = 'Products';
+$_['tab_quotes']            = 'Quotes';
+$_['tab_orders']            = 'Orders';
+$_['tab_delivery']          = 'Delivery Notes';
+$_['tab_invoice']           = 'Invoices';
+$_['tab_notes']             = 'Notes';
+$_['tab_info']              = 'Info';
+$_['tab_various']           = 'Various';
+
+// Column
+
+$_['column_name']           = 'Customer Name';
+$_['column_email']          = 'E-Mail';
+$_['column_quote']          = 'Quote Nº';
+
+$_['column_customer_group'] = 'Customer Group';
+
+$_['column_status']         = 'Status'; 
+
+$_['column_login']          = 'Login into Store';
+
+$_['column_approved']       = 'Approved';
+
+$_['column_date_added']     = 'Date Added';
+
+$_['column_comment']        = 'Comment';
+
+$_['column_description']    = 'Description';
+
+$_['column_amount']         = 'Amount';
+
+$_['column_points']         = 'Points';
+
+$_['column_ip']             = 'IP';
+
+$_['column_total']          = 'Total Accounts';
+
+$_['column_action']         = 'Action';
+
+$_['column_invoice']        = 'Invoice Nº';
+
+$_['column_delivery']       = 'Delivery Nº';
+
+$_['column_order']          = 'Order Nº';
+
+$_['column_email']          = 'Emails';
+
+$_['column_email_subject']  = 'Subject';
+
+$_['column_email_text']     = 'Text';
+
+$_['column_product_id']     = 'ID';
+
+$_['column_product_name']   = 'Product';
+
+$_['column_order_date']     = 'Date';
+
+$_['column_quantity']       = 'Quantity';
+
+$_['column_customer_id']    = 'ID';
+
+// Entry
+
+$_['entry_firstname']       = 'First Name:';
+
+$_['entry_lastname']        = 'Last Name:';
+
+$_['entry_email']           = 'E-Mail:';
+
+$_['entry_telephone']       = 'Telephone:';
+
+$_['entry_fax']             = 'Fax:';
+
+$_['entry_newsletter']      = 'Newsletter:';
+
+$_['entry_customer_group']  = 'Customer Group:';
+
+$_['entry_status']          = 'Status:';
+
+$_['entry_password']        = 'Password:';
+
+$_['entry_confirm']         = 'Confirm:';
+
+$_['entry_company']         = 'Company:';
+
+$_['entry_company_id']      = 'Company ID:';
+
+$_['entry_tax_id']          = 'Tax ID:';
+
+$_['entry_address_1']       = 'Address 1:';
+
+$_['entry_address_2']       = 'Address 2:';
+
+$_['entry_city']            = 'City:';
+
+$_['entry_postcode']        = 'Postcode:';
+$_['entry_country']         = 'Country:';
+$_['entry_zone']            = 'Region / State:';
+$_['entry_default']         = 'Default Address:';
+$_['entry_comment']         = 'Comment:';
+$_['entry_description']     = 'Description:';
+$_['entry_amount']          = 'Amount:';
+$_['entry_points']          = '<a data-toggle="tooltip" title="Use minus to remove points"><i class="fas fa-question-circle"></i></a> Points:';
+$_['text_datecreated']      = 'Created:' ;
+$_['text_date_modified']    = 'Last modified:' ;
+$_['text_date_support']     = 'Date Support:' ;
+$_['text_bank']     		= 'Bank:';
+$_['text_bic']   		    = 'BIC:';
+$_['text_fiscal']			= 'Fiscal:';
+$_['text_receptor']			= 'Receptor:';
+$_['text_paid']				= 'Paid Out:';
+
+// Error
+
+$_['error_warning']         = 'Warning: Please check the form carefully for errors!';
+
+$_['error_permission']      = 'Warning: You do not have permission to modify customers!';
+
+$_['error_exists']          = 'Warning: E-Mail Address is already registered!';
+
+$_['error_firstname']       = 'First Name must be between 1 and 32 characters!';
+
+$_['error_lastname']        = 'Last Name must be between 1 and 32 characters!';
+
+$_['error_email']           = 'E-Mail Address does not appear to be valid!';
+
+$_['error_telephone']       = 'Telephone must be between 3 and 32 characters!';
+
+$_['error_password']        = 'Password must be between 4 and 20 characters!';
+
+$_['error_confirm']         = 'Password and password confirmation do not match!';
+
+$_['error_company_id']      = 'Company ID required!';
+
+$_['error_tax_id']          = 'Tax ID required!';
+
+$_['error_vat']             = 'VAT number is invalid!';
+
+$_['error_address_1']       = 'Address 1 must be between 3 and 128 characters!';
+
+$_['error_city']            = 'City must be between 2 and 128 characters!';
+
+$_['error_postcode']        = 'Postcode must be between 2 and 10 characters for this country!';
+
+$_['error_country']         = 'Please select a country!';
+
+$_['error_zone']            = 'Please select a region / state!';
+
+// contacts
+$_['tab_contacts'] = 'Contacts';
+$_['column_name'] = 'Name';
+$_['column_telephone'] = 'Telephone';
+$_['column_contact_email'] = 'Email';
+$_['column_date'] = 'Date';
+
+$_['button_add_contact'] = 'Add Contact';
+$_['text_delete'] = 'Delete';
+
+// contacts form
+
+$_['entry_name'] = 'Name:';
+$_['entry_telephone2'] = 'Telephone 2:';
+$_['entry_puesto'] = 'Job:';
+$_['entry_notas'] = 'Notes:';
+
+// contracts
+$_['tab_contracts'] = 'Contracts';
+$_['column_article'] = 'Article';
+$_['column_quantity'] = 'Quantity';
+$_['column_end_support'] = 'End Support';
+$_['button_add_contract'] = 'Add Contract';
+$_['entry_article'] = 'Article:';
+$_['entry_quantity'] = 'Quantity:';
+$_['entry_date'] = 'Date Purchased:';
+$_['entry_end_support'] = 'End Support:';
+$_['entry_notes'] = 'Notes:';
+
+// notes
+$_['heading_title_note'] = 'Note';
+$_['entry_user'] = 'User:';
+$_['entry_date_note'] = 'Date:';
+$_['column_user'] = 'User';
+$_['button_add_note'] = 'Add Note';
+$_['button_delete_note'] = 'Delete Note';
+
+// New Email
+$_['text_to'] = 'To:';
+$_['text_subject'] = 'Subject:';
+$_['text_message'] = 'Message:';
+
+$_['button_new_email'] = 'New Email';
+$_['button_send'] = 'Send';
+$_['button_upload'] = 'Upload';
+
+$_['text_alert_imap'] = 'Imap library is not installed!!';
+
+?>
