0baf89c1732cbed417afee1d252cf12769f1367c
AlbertRD <flashinvoice@gmail.com>
2019-01-09 17:24:08 +0100

    Update Customer Group

---
+++ admin/language/en-gb/sale/customer_group.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']             = 'Customer Group';
+
+// Text
+$_['text_success']              = 'Success: You have modified customer groups!';
+
+// Column
+$_['column_name']               = 'Customer Group Name';
+$_['column_sort_order']         = 'Sort Order';
+$_['column_action']             = 'Action';
+
+// Entry
+$_['entry_name']                = 'Customer Group Name:';
+$_['entry_description']         = 'Description:';
+$_['entry_approval']            = '<a data-toggle="tooltip" title="Customers must be approved by and administrator before they can login."><i class="fas fa-question-circle"></i></a> Approve New Customers:';
+$_['entry_company_id_display']  = '<a data-toggle="tooltip" title="Display a company no. field."><i class="fas fa-question-circle"></i></a> Display Company No.:';
+$_['entry_company_id_required'] = '<a data-toggle="tooltip" title="Select which customer groups must enter their company no. for billing addresses before checkout."><i class="fas fa-question-circle"></i></a> Company No. Required:';
+$_['entry_tax_id_display']      = '<a data-toggle="tooltip" title="Display a Tax ID. field for billing addresses."><i class="fas fa-question-circle"></i></a> Display Tax ID.:';
+$_['entry_tax_id_required']     = '<a data-toggle="tooltip" title="Select which customer groups must enter their Tax ID for billing addresses before checkout."><i class="fas fa-question-circle"></i></a> Tax ID Required:';
+$_['entry_sort_order']          = 'Sort Order:';
+
+// Error
+$_['error_permission']          = 'Warning: You do not have permission to modify customer groups!';
+$_['error_name']                = 'Customer Group Name must be between 3 and 32 characters!';
+$_['error_default']             = 'Warning: This customer group cannot be deleted as it is currently assigned as the default store customer group!';
+$_['error_store']               = 'Warning: This customer group cannot be deleted as it is currently assigned to %s stores!';
+$_['error_customer']            = 'Warning: This customer group cannot be deleted as it is currently assigned to %s customers!';
+?>
