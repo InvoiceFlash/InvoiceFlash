a3342011aa5c883637837ba17849d0049df7a433
alto <alto@fpress.com>
2026-07-17 18:40:20 +0200

    invoice status

---
+++ admin/language/es-es/localisation/invoice_status.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']    = 'Estado de Factura';
+
+// Text
+$_['text_success']     = '&Eacute;xito: &iexcl;Ha modificado los estados de factura!';
+
+// Column
+$_['column_status_id'] = 'ID Estado de Factura';
+$_['column_name']      = 'Nombre del Estado de Factura';
+$_['column_action']    = 'Acci&oacute;n';
+
+// Entry
+$_['entry_name']       = 'Nombre del Estado de Factura:';
+$_['entry_color']      = 'Color <a data-toggle="tooltip" title="Introduzca en sistema RGB (ej: 170, 169, 169)"><i class="fas fa-question-circle"></i></a>';
+
+// Error
+$_['error_permission'] = 'Advertencia: &iexcl;No tiene permiso para modificar estados de factura!';
+$_['error_name']       = '&iexcl;El Nombre del Estado de Factura debe tener entre 3 y 32 caracteres!';
+$_['error_default']    = 'Advertencia: &iexcl;Este estado de factura no se puede eliminar porque est&aacute; asignado actualmente como el estado de factura por defecto de la tienda!';
+$_['error_download']   = 'Advertencia: &iexcl;Este estado de factura no se puede eliminar porque est&aacute; asignado actualmente como el estado de descarga por defecto!';
+$_['error_store']      = 'Advertencia: &iexcl;Este estado de factura no se puede eliminar porque est&aacute; asignado actualmente a %s tiendas!';
+$_['error_invoice']    = 'Advertencia: &iexcl;Este estado de factura no se puede eliminar porque est&aacute; asignado actualmente a %s facturas!';
+?>
---
+++ admin/language/es-es/localisation/shipping.php
@@ -0,0 +1,21 @@
+<?php
+
+$_['heading_title']		= 'M&eacute;todos de Env&iacute;o';
+
+$_['text_success']		= '&Eacute;xito: &iexcl;Ha modificado el env&iacute;o!';
+$_['text_no_results']	= '&iexcl;Sin resultados!';
+
+$_['column_name']		= 'Nombre';
+$_['column_action']		= 'Acci&oacute;n';
+
+$_['button_insert']		= 'Insertar';
+$_['button_delete']		= 'Eliminar';
+$_['button_save']		= 'Guardar';
+$_['button_cancel']		= 'Cancelar';
+
+$_['entry_name']		= 'Nombre:';
+
+$_['error_warning']		= 'Advertencia: &iexcl;No tiene permiso para modificar env&iacute;os!';
+$_['error_name']		= '&iexcl;El Nombre debe tener entre 3 y 32 caracteres!';
+
+ ?>
