08daf75ed479e0b80f7b1129e32871e6fe0949fd
YorkFPress <usualcuiono@gmail.com>
2026-09-22 11:39:17 +0200

    Otro

--- admin/controller/tool/mod_manager.php
+++ admin/controller/tool/mod_manager.php
@@ -43,53 +43,7 @@ class ControllerToolModManager extends Controller {
 			$this->data['error'] = '';
 		}
 
-		$this->load->model('setting/extension');
-
-		$extensions = $this->model_setting_extension->getInstalled('module');
-
-		foreach ($extensions as $key => $value) {
-			if (!file_exists(DIR_APPLICATION . 'controller/module/' . $value . '.php')) {
-				$this->model_setting_extension->uninstall('module', $value);
-
-				unset($extensions[$key]);
-			}
-		}
-
 		$this->data['extensions'] = array();
-
-		$files = glob(DIR_APPLICATION . 'controller/module/*.php');
-
-		if ($files) {
-			foreach ($files as $file) {
-				$extension = basename($file, '.php');
-
-				$this->language->load('module/' . $extension);
-
-				$action = array();
-
-				if (!in_array($extension, $extensions)) {
-					$action[] = array(
-						'text' => $this->language->get('text_install'),
-						'href' => $this->url->link('tool/mod_manager/install', 'token=' . $this->session->data['token'] . '&extension=' . $extension, 'SSL')
-					);
-				} else {
-					$action[] = array(
-						'text' => $this->language->get('text_edit'),
-						'href' => $this->url->link('module/' . $extension . '', 'token=' . $this->session->data['token'], 'SSL')
-					);
-
-					$action[] = array(
-						'text' => $this->language->get('text_uninstall'),
-						'href' => $this->url->link('tool/mod_manager/uninstall', 'token=' . $this->session->data['token'] . '&extension=' . $extension, 'SSL')
-					);
-				}
-
-				$this->data['extensions'][] = array(
-					'name'   => $this->language->get('heading_title'),
-					'action' => $action
-				);
-			}
-		}
 
 		// VQMod XML modules (activos y desactivados)
 		$vqmod_dir    = dirname(DIR_APPLICATION) . '/vqmod/xml/';
